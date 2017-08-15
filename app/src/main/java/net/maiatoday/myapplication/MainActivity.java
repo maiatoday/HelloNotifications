df0a79e3b28fb6da4d95f15b6d8b847729f1d9c0
maiatoday <maiatoday@gmail.com>
2017-08-15 17:23:51 +0200

    Skeleton app before fcm messaging :skull:

---
+++ app/src/main/java/net/maiatoday/myapplication/MainActivity.java
@@ -0,0 +1,34 @@
+package net.maiatoday.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button logTokenButton = (Button) findViewById(R.id.logTokenButton);
+        logTokenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                // Get token
+//                String token = FirebaseInstanceId.getInstance().getToken();
+//
+//                // Log and toast
+//                String msg = getString(R.string.msg_token_fmt, token);
+                String msg = "Strength of the pack!";
+                Log.d(TAG, msg);
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
