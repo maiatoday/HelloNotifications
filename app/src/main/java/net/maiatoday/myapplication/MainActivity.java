ffa41e98955890b03dedfcb673194ee401c80858
maiatoday <maiatoday@gmail.com>
2017-08-16 13:48:23 +0200

    test notifications with annotations

--- app/src/main/java/net/maiatoday/myapplication/MainActivity.java
+++ app/src/main/java/net/maiatoday/myapplication/MainActivity.java
@@ -1,38 +1,39 @@
 package net.maiatoday.myapplication;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        TextView tv = findViewById(R.id.textView);
-        tv.setText("Hello World " + BuildConfig.FLAVOR+" "+BuildConfig.BUILD_TYPE);
-        Button logTokenButton = (Button) findViewById(R.id.logTokenButton);
-        logTokenButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Get token
-                String token = FirebaseInstanceId.getInstance().getToken();
+    @ViewById(R.id.textView)
+    TextView textView;
 
-                // Log and toast
-                String msg = getString(R.string.msg_token_fmt, token);
+    @Click
+    void logTokenButton() {
+        String token = FirebaseInstanceId.getInstance().getToken();
+
+        // Log and toast
+        String msg = getString(R.string.msg_token_fmt, token);
 //                String msg = "Strength of the pack!";
-                Log.d(TAG, msg);
-                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
-            }
-        });
+        Log.d(TAG, msg);
+        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
     }
+
+    @AfterViews
+    void init() {
+        textView.setText("Hello World " + BuildConfig.FLAVOR + " " + BuildConfig.BUILD_TYPE);
+    }
+
 }
